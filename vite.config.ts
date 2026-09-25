7e5a5ca4eded369212d49c9702c24f7f70d82e48
kushalnggk08 <kushalnggk008@gmail.com>
2026-09-25 11:41:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base:'/Gym_Streak_Web_APP/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
